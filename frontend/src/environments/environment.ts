271d78a61eea4269b6e82adc5f19548fb109a89e
yousef <yousefelabd35@gmail.com>
2026-08-26 14:20:18 +0300

    fix: update production apiUrl in environment.ts to https://taqyeemy-backend.vercel.app

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
-﻿export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:3000'
+export const environment = {
+  production: true,
+  apiUrl: 'https://taqyeemy-backend.vercel.app'
 };
